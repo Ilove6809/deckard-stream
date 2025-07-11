9a6566ce6b76b989a3b4c09a802a8c1805c39562
Ilove6809 <144058157+Ilove6809@users.noreply.github.com>
2025-07-11 23:53:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -32,8 +32,7 @@ app.post('/incoming', (req, res) => {
   try {
     const response = new VoiceResponse();
     const connect  = response.connect();
-    connect.stream({ url: `wss://${process.env.SERVER}/connection` });
-
+    connect.stream({ url: `wss://${process.env.SERVER}/connection`, track: 'both' });
     res.type('text/xml');
     res.end(response.toString());
   } catch (err) {
